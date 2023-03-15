e4887fe58be802d8c4bb539c28b18126b2b4a243
Marcin Grygierek <grygierek.marcin@gmail.com>
2023-03-15 14:14:56 +0100

    TaskManager - pipe

--- TaskManager/src/app/shared/shared.module.ts
+++ TaskManager/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageContainerComponent } from './page-container/page-container.component';
+import { TimePipe } from './time.pipe';
 
 @NgModule({
   declarations: [
-    PageContainerComponent
+    PageContainerComponent,
+    TimePipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    PageContainerComponent
+    PageContainerComponent,
+    TimePipe
   ]
 })
 export class SharedModule { }
---
+++ TaskManager/src/app/shared/time.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'time'
+})
+export class TimePipe implements PipeTransform {
+
+  transform(value: number): string {
+    const minutes = Math.floor(value / 60);
+    const seconds = value % 60;
+
+    // const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    // const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    // return `${formattedMinutes}:${formattedSeconds}`;
+
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  }
+
+}
